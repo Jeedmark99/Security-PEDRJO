f8c0bab9d4b0ad61e69039755b5da9e4c35b1c5e
Jeedmark99 <jeedmark@icloud.com>
2023-01-28 18:34:04 +0100

    la till betygs-kraven för varje sida i kommentar

--- admin/js/manage-products.js
+++ admin/js/manage-products.js
@@ -1,1 +1,18 @@
 // File might not be sent to github if it doesn't contain anything, so I added a comment.
+
+/**
+● admin/manage-products.html
+○ En länk högst upp på sidan, som navigerar till products.html,
+○ Knapp/länk som navigerar till admin/create-product.html
+○ En lista på alla produkter i tabellform <table>
+○ Tabellen skall ha följande kolumner
+■ Title
+■ Price
+■ Stock
+■ Date
+■ Hantera: Innehåller 2 knappar:
+● Knapp/länk redigera befintlig produkt. Navigerar till
+admin/update-product.html
+● Knapp/länk raderar befintlig produkt
+ 
+ */
--- js/products.js
+++ js/products.js
@@ -1,1 +1,12 @@
 // File might not be sent to github if it doesn't contain anything, so I added a comment.
+/**
+ * Följande är obligatoriska delar för G-nivå:
+● products.html:
+○ En länk högst upp på sidan, som navigerar till admin sidan, dvs till
+admin/manage-products.html
+○ Visa en lista på alla produkter. Varje produkt skall visa minst
+■ Title
+■ Price
+
+ * 
+ */
