89a462a86415387c8457efe722e7432bd9a39448
Rayhana E <florayana97@hotmail.com>
2023-01-29 15:32:38 +0100

    create-update js code & html

--- admin/js/create-product.js
+++ admin/js/create-product.js
@@ -1,16 +1,53 @@
-// File might not be sent to github if it doesn't contain anything, so I added a comment.
+async function createProduct() {
 
-/**
- ● admin/create-product.html
-○ Ett formulär för att skapa nya produkter. Skall ha minst följande komponenter
-■ <Input> för title
-■ <textarea> för description
-■ <input> för price
-■ <input> för stock
-■ Knapp för att skapa produkten
-■ En tillbaka-länk för att navigera tillbaka till
-admin/manage-products.html
-○ Om formuläret lyckades skapa en ny produkt, skicka vidare användaren till
-admin/manage-products.html
+    checkAccessToken()
 
- */
+    try {
+        document.getElementById('create-product-form').addEventListener('submit', async function(e) {
+            e.preventDefault();
+            console.log(e.target);
+
+            let formData = new FormData(e.target); 
+            let formDataObject = {
+                content: formData.get('content')
+            }
+
+            console.log(JSON.stringify(formDataObject));
+
+
+            const response2 = await fetch(ROOT_URL + '/products' , {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+                },
+                body: JSON.stringify(formDataObject)
+            })
+           // location.replace('index.html');
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+createProduct();
+
+let serializeForm = function (form) {
+    var obj = {};
+    var formData = new FormData(form);
+    // console.log(formData.getAll());
+
+    for (var key of formData.keys()) {
+        let inputData = formData.getAll(key);
+
+        if (inputData.length > 1) {
+            obj[key] = inputData;
+        } else {
+            obj[key] = inputData[0];    
+        }
+    }
+    
+    // console.log(obj);
+    return JSON.stringify(obj);
+};
--- admin/js/update-product.js
+++ admin/js/update-product.js
@@ -1,11 +1,81 @@
-// File might not be sent to github if it doesn't contain anything, so I added a comment.
 
-/**
- ● admin/update-product.html
-○ Ett formulär för att uppdatera befintliga produkter. Har samma komponenter
-som create-product.html formuläret. Skillnaden är att alla fält är förifyllda med
-den befintliga produktens data.
-○ Om formuläret lyckades skapa en ny produkt, skicka vidare användaren till
-admin/manage-products.html
 
- */
+console.log(window.location.search);
+let urlParams = new URLSearchParams(window.location.search)
+console.log(urlParams);
+console.log(urlParams.get('id'));
+
+
+async function updateProduct() {
+    checkAccessToken();
+
+    try {
+        const response = await fetch(ROOT_URL + urlParams.get('id'), {
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('accessToken')
+            }
+        });
+        const pun = await response.json()
+        console.log(pun)
+        document.getElementById('content-textarea').value = product.content;
+    } catch (error) {
+        console.log(error);
+    }
+}
+updateProduct()
+
+async function createProduct() {
+    checkAccessToken()
+
+
+    try {
+        document.getElementById('update-product-form').addEventListener('submit', async function(e) {
+            e.preventDefault();
+            console.log(e.target);
+
+            let formData = new FormData(e.target); // e.target is the form that is being submitted. Need to send in the whole form when using FormData
+            let formDataObject = {
+                content: formData.get('content')
+            }
+            console.log(JSON.stringify(formDataObject));
+
+            const response2 = await fetch(ROOT_URL + '/products' + urlParams.get('id'), {
+                method: 'PATCH', // *GET, POST, PUT, DELETE, etc.
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('accessToken')
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: JSON.stringify(formDataObject) // body data type must match "Content-Type" header
+            });
+
+            location.replace('index.html'); // Redirect to index.html
+        })
+
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+createProduct();
+
+
+let serializeForm = function (form) {
+    var obj = {};
+    var formData = new FormData(form);
+    // console.log(formData.getAll());
+
+    for (var key of formData.keys()) {
+        let inputData = formData.getAll(key);
+
+        if (inputData.length > 1) {
+            obj[key] = inputData;
+        } else {
+            obj[key] = inputData[0];    
+        }
+    }
+    
+    // console.log(obj);
+    return obj;
+};
