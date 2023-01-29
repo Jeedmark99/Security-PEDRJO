70c6d91d74a6c333c717bb50acad945b2e931aaa
Jeedmark99 <jeedmark@icloud.com>
2023-01-29 13:55:36 +0100

    some code for the delete request

--- admin/js/manage-products.js
+++ admin/js/manage-products.js
@@ -44,14 +44,45 @@ async function fetchAllProducts(){
 
         let productsHTML = "";
         for (let product of products){
-            productsHTML += ` <tbody> <td>${product.title}</td> <td>${product.description}</td> <td>${product.price}</td> <td>${product.stock}</td> <td>${product.category}</td> <td>${product.date}</td> <td> <a href="#" class="edit-links">Edit</a> <a href="#" class="delete-links">Delete</a></td> </tbody> `;
+            productsHTML += ` <tbody> <td>${product.title}</td> <td>${product.description}</td> <td>${product.price}</td> <td>${product.stock}</td> <td>${product.category}</td> <td>${product.date}</td> <td> <a href="#" class="edit-links">Edit</a> <a href="#" class="delete-products-links" data-id=${product._id}>Delete</a></td> </tbody> `;
         }
 
         document.getElementById('manage-products-table').innerHTML += productsHTML
 
     } catch (error){
-        console.log(error);{}
+        console.log(error);
     }
+
+    //////////////////////////////////////////////////////////////////
+    //                      MANAGE DELETE                           //
+    const deleteLinks = document.getElementsByClassName('delete-products-links');
+    console.log(deleteLinks);
+
+    for(let deleteLink of deleteLinks){
+        deleteLink.addEventListener('click', async function(e){
+            e.preventDefault()
+
+            console.log(e.target);
+            console.log(e.target.dataset.id) //product id
+
+
+            try{
+                const response3 = await fetch('http://localhost:5000/products/' + e.target.dataset.id, {
+                    method: 'DELETE',
+                    headers:{
+                        'Content-Type':'application/json',
+                        'Authorization': 'Bearer ' + data.accessToken
+                    }
+                });
+                console.log(response3);
+                console.log(e.target.parentNode)
+
+            } catch (error){
+                console.log(error);
+            }
+        })
+    }
+
 }
 
 fetchAllProducts();
