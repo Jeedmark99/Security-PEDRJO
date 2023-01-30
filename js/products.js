546be0a08d107915b4c7ed320a8ebd6fcda2037a
Linn <linnvailt@gmail.com>
2023-01-30 16:12:40 +0100

    products update

--- js/products.js
+++ js/products.js
@@ -10,3 +10,47 @@ admin/manage-products.html
 
  * 
  */
+
+
+/*
+
+async function fetchAllProducts(){
+    try{
+        const response1 = await fetch('http://localhost:5000/api-users/token%27,%7B
+            method:'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({
+                username: "user1",
+                password: "whatever123"
+            }) 
+        });
+
+        const data = await response1.json();
+        console.log(data.accessToken);
+
+        const response2 = await fetch ('http://localhost:5000/products', {
+            method:"GET",
+            headers:{
+                'Authorization': 'Bearer ' + data.accessToken
+            }
+
+        });
+        const products = await response2.json()
+        console.log(products);
+
+        let productsHTML = "";
+        for (let product of products){
+            let productDate = new Date(product.date)
+            productsHTML += <div> <tbody class = "productList"> <td>${product.title}</td> <td>${product.description}</td> <td>${product.price}</td> <td>${product.stock}</td> <td>${product.category}</td> <td>${productDate.toLocaleDateString()}</td> <td> <a href="#" class="edit-links">Edit</a> <a href="#" class="delete-products-links" data-id=${product._id}>Delete</a></td> </tbody> </div>;
+        }
+
+        document.getElementById('manage-products-table').innerHTML += productsHTML
+
+    } catch (error){
+        console.log(error);
+    }
+fetchAllProducts();
+
+*/
